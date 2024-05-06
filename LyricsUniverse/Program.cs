16c3d91419859b25e2eca01eaff6eaccada2166e
1nsenter <1nsenter@mail.ru>
2024-05-06 19:31:37 +0300

    fix start page

--- LyricsUniverse/Program.cs
+++ LyricsUniverse/Program.cs
@@ -60,13 +60,9 @@ namespace LyricsUniverse
             app.UseAuthentication();
             app.UseAuthorization();
 
-            //app.MapControllerRoute(
-            //    name: "default",
-            //   pattern: "{controller=Home}/{action=Index}/{id?}");
-
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Admin}/{action=Index}/{id?}");
+               pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
         }
